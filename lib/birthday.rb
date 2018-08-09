3605a0f9e631773e2a523c9401c9841571cbfc0a
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-09 17:08:11 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,4 +4,9 @@
 #   "Amanda" => 27
 # }
 
-winner
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
