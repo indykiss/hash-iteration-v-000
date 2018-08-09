43af80a1d372d9f5274108890e36f29c716ffe9e
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-09 17:00:35 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,9 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  
+  passengers.each do |value, key|
+    if key = "suite_a" && value[0] == "A"
+      return value, key
+      
 end
